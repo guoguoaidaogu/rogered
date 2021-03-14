32b80f97f9c8ef8d1f64df8472b55e3cd269ebe6
guoguoaidaogu <guoguoaidaogu@163.com>
2021-03-14 13:07:38 +0800

    Renamed something

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cstdio>
+#include "gstdlib.cpp"
+#include "gconeng.cpp"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/* int argc, char** argv */
+using namespace std;
 
 int main() {
 	return 0;
 }
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* int argc, char** argv */
